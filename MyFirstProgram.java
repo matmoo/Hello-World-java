0e29c3d626872833ef92e9be466c2bd4de2034c3
Mateo Mora <moramateo65@gmail.com>
2025-01-18 14:43:28 -0800

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,17 @@
+/*
+This is my first Java program.
+Author: Mateo Mora
+Date: 1/8/2025
+File: MyFirstProgram.java
+*/
+// Java program to print “Hello World!” to the screen
+public class MyFirstProgram
+{
+//the main method
+   public static void main(String[] args)
+   {
+// invoke println method from System.out that require one argument
+// i.e. item to be printed
+      System.out.println("Hello, World!");
+   }
+}
